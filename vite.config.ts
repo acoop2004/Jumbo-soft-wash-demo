1cff84106c213c5aceab451f75b577cbb0e5dfa6
acoop2004 <alexismariec04@gmail.com>
2026-06-10 16:57:57 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
-import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
@@ -10,20 +10,6 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
-      },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
-      watch: process.env.DISABLE_HMR === 'true' ? null : {},
-    },
-    build: {
-      assetsDir: 'assets',
-      rollupOptions: {
-        output: {
-          assetFileNames: '[name]-[hash][extname]',
-          chunkFileNames: '[name]-[hash].js',
-          entryFileNames: '[name]-[hash].js',
-        },
       },
     },
   };
