2415b2a2a878a6e862806e6d9729b7d14a373a27
acoop2004 <alexismariec04@gmail.com>
2026-06-10 16:53:45 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig(() => {
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
     build: {
-      assetsDir: '',
+      assetsDir: 'assets',
       rollupOptions: {
         output: {
           assetFileNames: '[name]-[hash][extname]',
